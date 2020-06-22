d9e5a39ae89cc4e736a3e013d5f43131a0eeedfb
Ryan <ryansherring@gmail.com>
2020-06-22 13:36:40 -0700

    Updates

---
+++ precache-manifest.430811cf58a406b3f79e2ad34fed432e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7a86c6e494473ddf7a412a44e55b3c",
+    "url": "/github-buddy/index.html"
+  },
+  {
+    "revision": "433950e9d8fb7be27e91",
+    "url": "/github-buddy/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1d8ceb94ac2f72c80489",
+    "url": "/github-buddy/static/css/main.f45226ac.chunk.css"
+  },
+  {
+    "revision": "433950e9d8fb7be27e91",
+    "url": "/github-buddy/static/js/2.a8275b6a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/github-buddy/static/js/2.a8275b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8ceb94ac2f72c80489",
+    "url": "/github-buddy/static/js/main.8fa125e8.chunk.js"
+  },
+  {
+    "revision": "fd8f9e026cbd63fec2bc",
+    "url": "/github-buddy/static/js/runtime-main.7059fd2f.js"
+  },
+  {
+    "revision": "3960c1e79e98d7dc929a7fd0ad45f71e",
+    "url": "/github-buddy/static/media/background.3960c1e7.jpg"
+  }
+]);
